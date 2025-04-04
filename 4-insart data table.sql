5e57806c75a52661f800aaf3aff2a23b505f0864
noni022 <aafmsportnoni@gmail.com>
2025-04-04 17:00:50 +0530

    just print the employee data

--- 4-insart data table.sql
+++ 4-insart data table.sql
@@ -1,4 +1,12 @@
 select * from employee;
 
 insert into employee(name, postition, hire_date, saleary)
-		values ('ajit sharma', 'data analyst', '200-05-25' , 6500.00);
+		values ('ajit sharma', 'data analyst', '200-05-25' , 6500.00),
+				('souraj ', 'data securty', '03-04-2025' , 38000.00);
+
+
+truncate table employee;	
+
+truncate table employee restart identity;
+
+select * from employee;
